7467d1470002693664f08dd1687970279c937b2a
Johann Gomez <johanngomez02@gmail.com>
2021-10-07 15:39:18 -0500

    show all, delete all, delete one, checked, add item

---
+++ list.rb
@@ -0,0 +1,36 @@
+require "./item.rb"
+
+class List
+    attr_writer :items
+
+    def initialize()
+        @items = Array.new()
+    end
+
+    def add_item(text)
+        item = Item.new(text)
+        @items.push(item)
+    end
+
+    def remove_item(index)
+        @items.delete_at(index)
+    end
+
+    def check_item(index)
+        @items[index].checked = true
+    end
+
+    def remove_all
+        @items.clear
+    end
+
+    def show_all
+        if @items.length == 0
+            puts "No hay articulos en tu lista de compras"
+        else
+            @items.each_index do |index|
+                puts index.to_s + " - " + @items[index].to_s
+            end
+        end
+    end
+end
