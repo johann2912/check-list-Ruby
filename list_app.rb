2aa9bb6fb934212237354dc5c7ae2f14ef0712ce
Johann Gomez <johanngomez02@gmail.com>
2021-10-07 16:10:05 -0500

    creating user menu, exiting the functional application

--- list_app.rb
+++ list_app.rb
@@ -1,6 +1,13 @@
 require "./list.rb"
 require "./item.rb"
 
+# Agregate article
+# Remove article
+# Show All articles
+# Checked article
+# Deleted All articles
+# exit application
+
 class ListApp
     attr_writer :list
 
@@ -10,8 +17,22 @@ class ListApp
 
     def run
         puts "Welcome to our shopping list"
-    end
+        loop do
+            puts "A - Add an item"
+            puts "R - Remove an article"
+            puts "V - Show all items"
+            puts "C - Check an article"
+            puts "D - Delete all articles"
+            puts "E - Exit"
+            input = gets.chomp
 
+            case input.upcase
+            when 'E'
+                break
+            end
+        end 
+        puts "thank you for using our application"
+    end
 end
 
 list_app = ListApp.new
