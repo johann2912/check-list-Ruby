025cbb22d2e49a4a5a30db3706c004d5d51cb1af
Johann Gomez <johanngomez02@gmail.com>
2021-10-07 17:06:34 -0500

    completed application

--- list_app.rb
+++ list_app.rb
@@ -20,13 +20,40 @@ class ListApp
         loop do
             puts "A - Add an item"
             puts "R - Remove an article"
-            puts "V - Show all items"
+            puts "S - Show all items"
             puts "C - Check an article"
             puts "D - Delete all articles"
             puts "E - Exit"
             input = gets.chomp
 
             case input.upcase
+            when 'A'
+                puts "what would you like to add?"
+                item = gets.chomp
+                @list.add_item(item)
+
+            when 'R'
+                puts "which item you want to remove?"
+                index = gets.chomp
+                @list.remove_item(index.to_i)
+            
+            when 'S'
+                @list.show_all
+            
+            when 'C'
+                puts "which item you want to mark "
+                index = gets.chomp
+                @list.check_item(index.to_i)
+            
+            when 'D'
+                puts "do you want to remove all items? y/n"
+                input = gets.chomp
+                if input.upcase == 'Y'
+                    @list.remove_all
+                else
+                    puts "cancelled operation"
+                end
+            
             when 'E'
                 break
             end
