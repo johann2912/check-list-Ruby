a3bfed139f5cca1564e13f3b4a72ed2bd41f1ccc
Johann Gomez <johanngomez02@gmail.com>
2021-10-07 17:27:34 -0500

    agregating messages for user

--- list_app.rb
+++ list_app.rb
@@ -18,6 +18,7 @@ class ListApp
     def run
         puts "Welcome to our shopping list"
         loop do
+            puts
             puts "A - Add an item"
             puts "R - Remove an article"
             puts "S - Show all items"
@@ -28,24 +29,39 @@ class ListApp
 
             case input.upcase
             when 'A'
+                puts
                 puts "what would you like to add?"
                 item = gets.chomp
                 @list.add_item(item)
+                30.times { print "*" }
+                puts "\n#{item} has been added to your list"
+                30.times { print "*" }
 
             when 'R'
+                puts
                 puts "which item you want to remove?"
                 index = gets.chomp
                 @list.remove_item(index.to_i)
+                30.times { print "*" }
+                puts "\n#{index} has been removed from your list"
+                30.times { print "*" }
             
             when 'S'
+                puts
                 @list.show_all
-            
+                30.times { print "*" }
+                puts "\nthese are all the items on your list"
+                30.times { print "*" }
             when 'C'
+                puts
                 puts "which item you want to mark "
                 index = gets.chomp
-                @list.check_item(index.to_i)
-            
+                @list.check_item(index.to_i) 
+                30.times { print "*" }
+                puts "\n#{index} has been marked"
+                30.times { print "*" }
             when 'D'
+                puts
                 puts "do you want to remove all items? y/n"
                 input = gets.chomp
                 if input.upcase == 'Y'
