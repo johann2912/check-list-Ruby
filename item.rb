4ee9c1637ddf748c877b78be663fdfaf0f6f708e
Johann Gomez <johanngomez02@gmail.com>
2021-10-07 14:03:35 -0500

    initialize, to_s

--- item.rb
+++ item.rb
@@ -1,1 +1,17 @@
-puts "Hola mundo"
+class Item
+    attr_writter :text, :checked
+
+    def initialize(text="")
+        @text = text
+        @checked = false
+    end
+
+    def to_s
+        if @checked
+            "[x]" + @text
+        else
+            "[ ]" + @text
+        end
+    end
+    
+end
